de598da1a6f1827e6854de4bee5b35f01895a9d1
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-26 11:01:19 +0100

    add experiment with while loop

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -9,7 +9,7 @@ for (index = 0; index < 10; index++){
 } */
 
 /* made with while loop */
-let sum = 0;
+/* let sum = 0;
 let index = 1;
 
 while(index < 11){
@@ -18,4 +18,14 @@ while(index < 11){
   sum += addNumb;
   console.log("count number: " + index, "the current sum is: " + sum)
   index++;
+} */
+
+/* experiment: while sum is less than 100 */
+let sum = 0;
+
+while(sum <100){
+  let number = prompt("add a number");
+  let addNumb = parseFloat(number);
+  sum += addNumb;
+  console.log("the sum is: " + sum)
 }
