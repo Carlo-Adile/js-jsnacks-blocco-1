d85965cf0ef08231a934df8fde96875bf8336c39
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-26 12:05:19 +0100

    add comments

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -4,11 +4,11 @@ se è dispari inseriscilo nell’array. */
 let oddNumbs = [];
 
 for (index = 0; index < 6; index++){
+  /* add numb and parse to a float number */
   let addNumb = prompt("add a number, odd numbers will be stored");
-
   let canAdd = parseFloat(addNumb);
   
-  
+  /* if the prompt is a number and is odd, add it to the array */
   if(canAdd % 2 !== 0 && isNaN(canAdd) == false){
     oddNumbs.push(canAdd);
   }
@@ -17,4 +17,5 @@ for (index = 0; index < 6; index++){
   }
 }
 
+/* console log the collected numbers */
 console.log("your final collection of odd numbers is: " + oddNumbs + ". you stored a total of " + oddNumbs.length + " odd numbers")
