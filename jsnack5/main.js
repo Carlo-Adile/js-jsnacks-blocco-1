e12accc443bbb0976a86efe727f72112387fd8b1
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-26 11:47:09 +0100

    snack 5 for loop

---
+++ jsnack5/main.js
@@ -0,0 +1,15 @@
+/* Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array. */
+
+let oddNumbs = [];
+
+for (index = 0; index < 3; index++){
+  let addNumb = prompt("add a number, odd numbers will be stored");
+  if(addNumb % 2 !== 0){
+    oddNumbs.push(addNumb);
+  }
+  else{
+    console.log(addNumb + " was an even number")
+  }
+}
+console.log("your final collection of odd numbers is: " + oddNumbs)
