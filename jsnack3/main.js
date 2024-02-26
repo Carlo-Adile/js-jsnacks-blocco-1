12ebee449fb41a8857feec94e6568f6eb8f332ce
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-26 10:54:22 +0100

    snack 3 while loop

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,9 +1,21 @@
 /* made with for loop */
-let sum = 0;
+/* let sum = 0;
 for (index = 0; index < 10; index++){
   
   let number = prompt("add a number");
   let addNumb = parseFloat(number);
   sum += addNumb;
   console.log(sum)
+} */
+
+/* made with while loop */
+let sum = 0;
+let index = 0;
+
+while(index < 10){
+  let number = prompt("add a number");
+  let addNumb = parseFloat(number);
+  sum += addNumb;
+  console.log(sum)
+  index++;
 }
