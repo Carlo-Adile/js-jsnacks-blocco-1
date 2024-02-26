86b54a7cc646ab1a66acf046f55c1ad790bd55f7
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-26 12:12:36 +0100

    snack 6

---
+++ snack6/main.js
@@ -0,0 +1,8 @@
+/* Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente. */
+
+let userNumb = prompt("add a number to check the cubic value")
+
+let isNumb = parseFloat(userNumb)
+
+console.log("the cube value is " + (isNumb * isNumb))
